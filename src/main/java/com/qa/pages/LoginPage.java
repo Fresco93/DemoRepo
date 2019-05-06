f921017ffd1c9dbb15f91eab5cf6884f7b6dd980
karthirajan <kajekarthi@gmail.com>
2019-05-06 12:08:32 -0700

    search page code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,7 +1,10 @@
 package com.qa.pages;
 
 public class LoginPage {
-public void loginPage() {
-System.out.println("Login Page");
+    public void loginPage() {
+    System.out.println("Login Page");
+    }
+    public void loginSet() {
+        System.out.println("Login Set");
 }
 }
