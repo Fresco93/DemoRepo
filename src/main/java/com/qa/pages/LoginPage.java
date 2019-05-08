0125a016ec8fd50b01c1c75c46737acf9d4ab704
Fresco93 <48005174+Fresco93@users.noreply.github.com>
2019-05-08 11:21:41 -0700

    Update LoginPage.java

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -4,6 +4,7 @@ public class LoginPage {
     public void loginPage() {
     System.out.println("Login Page");
     }
+    jsdjksdc,,c
     public void loginSet() {
         System.out.println("Login Set");
         
